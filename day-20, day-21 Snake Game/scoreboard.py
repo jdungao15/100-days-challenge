b0bdaff5e4bf9c55b65f9dfb573fa69b1e887fe8
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2023-01-06 11:52:08 -0500

    refractor snake game

--- day-20, day-21 Snake Game/scoreboard.py
+++ day-20, day-21 Snake Game/scoreboard.py
@@ -1,11 +1,12 @@
 from turtle import Turtle
-ALIGMENT = "center"
+ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("White")
         self.penup()
         self.goto(0, 250)
@@ -13,13 +14,15 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score:{self.score} ", align=ALIGMENT, font=FONT)
+        self.clear()
+        self.write(f"Score:{self.score} High Score: {self.high_score} ", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()

e9d0bee5031d9a70d49f99ccbcba74c2075b9f3c
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2023-01-10 17:45:02 -0500

    finish UI

--- day-28-pomodoro/main.py
+++ day-28-pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -18,9 +19,24 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
-canvas.create_image()
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.grid(row=1, column=1)
 
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(row=2, column=2)
+
+timer_label = Label(text='TIMER', font=(FONT_NAME, 35, 'bold'), fg=GREEN, bg=YELLOW)
+timer_label.grid(row=0, column=1)
+
+check_label = Label(text='✔', fg=GREEN, bg=YELLOW)
+check_label.grid(row=3, column=1)
 
 window.mainloop()

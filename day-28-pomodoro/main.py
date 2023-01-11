3b37164e719e7c0906f01219566ffe366f7699db
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2023-01-10 19:40:42 -0500

    added timer feauture on day-28

--- day-28-pomodoro/main.py
+++ day-28-pomodoro/main.py
@@ -13,21 +13,32 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+
+window.after(1000)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill='white', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(100, 130, text="00:00", fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(row=1, column=1)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = Button(text="Reset", highlightthickness=0)
@@ -38,5 +49,6 @@ timer_label.grid(row=0, column=1)
 
 check_label = Label(text='✔', fg=GREEN, bg=YELLOW)
 check_label.grid(row=3, column=1)
+
 
 window.mainloop()

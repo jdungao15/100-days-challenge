7847b4e2e1d8c8a8ebe404faf9c4d4723e885591
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2023-01-15 22:21:28 -0500

    added get destination code functionality

--- day-39-flight-club/flight_search.py
+++ day-39-flight-club/flight_search.py
@@ -1,15 +1,17 @@
 import requests
+from pprint import pprint
 
 header = {
     'Content-Type': 'application/json',
     "apikey": 'WTiw1n3xxv-jS-t9bDZkkcduLFqrUZZN'
 }
-ENDPOINT = 'https://api.tequila.kiwi.com/v2/search'
-
+ENDPOINT = 'https://api.tequila.kiwi.com'
 
 class FlightSearch:
-
-
     def get_destination_code(self, city_name):
-        code = "Testing"
+        location_endpoint = f"{ENDPOINT}/locations/query"
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=header, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code

954ed2db7154356c86e8aea6de4ec482636ebc53
John Dungao <johnmiguel.dungao@gmail.com>
2023-01-19 17:33:14 -0500

    soup

--- day-45/main.py
+++ day-45/main.py
@@ -1,10 +1,29 @@
 from bs4 import BeautifulSoup
+import requests
 import pprint
 
-with open('website.html', encoding='utf-8') as file:
-    contents = file.read()
 
-soup = BeautifulSoup(contents, 'html.parser')
+# with open('website.html', encoding='utf-8') as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, 'html.parser')
+#
+# all_anchors_tags = soup.find_all(name="a")
+#
+# class_is_heading = soup.find_all(class_="heading")
+# print(class_is_heading)
+#
+# h3_heading = soup.find_all("h3", class_="heading")
+# print(h3_heading)
+#
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
 
-all_anchors_tags = soup.find_all(name="a")
-pprint.pprint(all_anchors_tags)
+response = requests.get("https://news.ycombinator.com/news")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, 'html.parser')
+title_list= soup.select(selector=".titleline a")
+for title in title_list:
+    print(title.text)
+

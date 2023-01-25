a4b55eb29f67d4d1c53b6add1d3fd17f40803b4c
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2023-01-25 18:00:38 -0500

    django

--- day-54/hello.py
+++ day-54/hello.py
@@ -1,6 +1,36 @@
 from flask import Flask
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        string = function()
+        return f"<b>{string}</b>"
+    return wrapper
+
+def make_emphasis(func):
+    def wrapper():
+        string = func()
+        return f"<em>{string}</em>"
+    return wrapper
+
+def make_italic(func):
+    def wrapper():
+        string = func()
+        return f"<i>{string}"
+    return wrapper
+
+
+
 @app.route('/')
 def hello_world():
     return "<h1>Hello worldHi World</h1>"
+
+
+@app.route('/bye')
+@make_italic
+def bye():
+    return "Bye"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

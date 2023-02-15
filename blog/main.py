2286bf42cb4497eec607c3385909dd19719c6593
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2023-02-14 21:09:53 -0500

    blog

--- blog/main.py
+++ blog/main.py
@@ -1,25 +1,101 @@
-from flask import Flask, render_template, request
-import requests
-from post import Post
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+import datetime
+
+
+## Delete this code:
+# import requests
+# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+##CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
 
 
 @app.route('/')
-def home():
-    res = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
-    blog_data = res.json()
-    all_posts = [Post(blog['title'], blog['subtitle'], blog['body'], blog['id']) for blog in blog_data]
-    return render_template("index.html", posts=all_posts)
+def get_all_posts():
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
 
 
-@app.route('/post/<post_id>')
-def get_post(post_id):
-    title = request.args.get('title')
-    subtitle = request.args.get('subtitle')
-    body = request.args.get('body')
+@app.route("/post/<int:index>")
+def show_post(index):
+    post = BlogPost.query.get(index)
+    return render_template("post.html", post=post)
 
-    return render_template('post.html', title=title, subtitle=subtitle, body=body)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/new_post", methods=["GET", "POST"])
+def add_new_post():
+    form = CreatePostForm()
+    date = datetime.datetime.now().strftime("%B %d, %Y")
+    if form.is_submitted():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            author=form.author.data,
+            img_url=form.img_url.data,
+            body=form.body.data,
+            date=date
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+    return render_template("make-post.html", form = form)
+
+#Edit Post
+@app.route("/edit-post/<int:post_id>", methods=["PUT"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    if request.method == "PUT":
+        data = request.get_json()
+        post.title = data["title"]
+        post.subtitle = data["subtitle"]
+        post.body = data["body"]
+        post.author = data["author"]
+        post.img_url = data["img_url"]
+        db.session.commit()
+        return jsonify(response={"success": "Post updated successfully."}), 200
+    return jsonify(response={"error": "Something went wrong."}), 500
 
 
 if __name__ == "__main__":
